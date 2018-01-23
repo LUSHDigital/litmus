f8516cb9a3a10fa3be204193dae328c35d4bc5b1
Benjamin Cable <cable.benjamin@gmail.com>
2018-01-23 10:26:29 +0000

    refactored format package

--- format/format.go
+++ format/format.go
@@ -1,6 +1,12 @@
 // Package format defines the format of a Litmus test file, as represented in code.
 package format
 
+import (
+	"bytes"
+	"github.com/tidwall/sjson"
+	"text/template"
+)
+
 // TestFile is the top level container element defining a Litmus test file
 type TestFile struct {
 	// Litmus is the top level table
@@ -44,4 +50,50 @@ func (c GetterConfigs) Filter(t string) (filtered GetterConfigs) {
 		}
 	}
 	return
+}
+
+func (r *RequestTest) ApplyEnv(env map[string]interface{}) (err error) {
+	if r.URL, err = applyTpl(r.URL, env); err != nil {
+		return
+	}
+	if r.Body, err = applyTpl(r.Body, env); err != nil {
+		return
+	}
+	for k, v := range r.BodyModifiers {
+		if r.Body, err = sjson.Set(r.Body, k, v); err != nil {
+			return
+		}
+	}
+	for k, v := range r.Headers {
+		if r.Headers[k], err = applyTpl(v, env); err != nil {
+			return
+		}
+	}
+	for k, v := range r.Query {
+		if r.Query[k], err = applyTpl(v, env); err != nil {
+			return
+		}
+	}
+	for i := range r.Getters {
+		if r.Getters[i].Expected, err = applyTpl(r.Getters[i].Expected, env); err != nil {
+			return
+		}
+		if r.Getters[i].Path, err = applyTpl(r.Getters[i].Path, env); err != nil {
+			return
+		}
+	}
+	return
+}
+
+func applyTpl(input string, env map[string]interface{}) (output string, err error) {
+	buf := &bytes.Buffer{}
+	t, err := template.New("anon").Parse(input)
+	if err != nil {
+		return "", err
+	}
+	if err = t.Execute(buf, env); err != nil {
+		return
+	}
+
+	return buf.String(), nil
 }
